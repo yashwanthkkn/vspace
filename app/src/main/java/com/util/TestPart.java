2477660ef9030796aba91fecdd061e93b8b201f8
Yashwanth K <yashwanthkkn@gmail.com>
2021-12-07 17:44:42 +0530

    bugfix : added testPart

---
+++ app/src/main/java/com/util/TestPart.java
@@ -0,0 +1,25 @@
+package com.util;
+
+import com.entities.Participation;
+import com.entities.Test;
+
+public class TestPart {
+	private Test test;
+	private Participation participation;
+	public TestPart(Test t, Participation p) {
+		this.test = t;
+		this.participation = p;
+	}
+	public Participation getParticipation() {
+		return participation;
+	}
+	public void setParticipation(Participation participation) {
+		this.participation = participation;
+	}
+	public Test getTest() {
+		return test;
+	}
+	public void setTest(Test test) {
+		this.test = test;
+	}
+}
