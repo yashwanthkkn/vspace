eadf49a08e9fa140fee0ff8fdc4ad68691021017
SriYazhinidevi <sriyazhinidevi12@gmail.com>
2021-12-14 10:32:04 +0530

    merge fixed

--- app/src/main/java/com/dao/UserDAOImpl.java
+++ app/src/main/java/com/dao/UserDAOImpl.java
@@ -19,18 +19,22 @@ import com.util.HibernateConfiguration;
 public class UserDAOImpl extends AbstractDAO<Integer, User> implements UserDAO{
 
 	@Override
-	public User findById(int uid) {
+	public User findById(int uid) { 
+		
 		return getByKey(uid);
+		
 	}
 
 	@Override
 	public void saveUser(User user) {
+		
 		persist(user);
 		
 	}
 
 	@Override
 	public void deleteUserById(int uid) {
+		
 		Criteria criteria =  createEntityCriteria();
         User user=(User)criteria.add(Restrictions.eq("uid", uid)).uniqueResult();
         delete(user);
@@ -39,15 +43,19 @@ public class UserDAOImpl extends AbstractDAO<Integer, User> implements UserDAO{
 
 	@Override
 	public List<User> findAllUsers() {
+		
 		Criteria criteria = createEntityCriteria();
         return (List<User>) criteria.list();
+        
 	}
 
 	@Override
 	public User findUserByEmailid(String emailid) {
+		
 		Criteria criteria = createEntityCriteria();
         criteria.add(Restrictions.eq("emailid", emailid));  
         return (User) criteria.uniqueResult();
+        
 	}
 	
 }
